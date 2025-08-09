456a9298f6aafaa932b5823b7d2bb00c54019baf
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-08-09 23:40:55 +0530

    set the bg

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="bg-[url(/bg.png)] bg-cover bg-center h-screen w-screen flex items-center justify-center ">
+        <h1 className="text-6xl font-extrabold text-[#00FF9E] ">
+          CODE KE BOSS 2025
+       </h1>
+    </div>
+  )
+}
+
+export default App
